d00cc8107caa1d3cea07edca9f25c7ca89aedc43
Thomas Lee <tjlee0927@gmail.com>
2020-09-15 22:25:25 -0400

    Problem 2

---
+++ Problem2.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+
+/****************************************** PROBLEM 2 QUESTION ******************************************
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.
+
+
+
+        Example 1:
+
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+        Example 2:
+
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+        Example 3:
+
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
+
+
+        Constraints:
+
+        2 <= nums.length <= 105
+        -109 <= nums[i] <= 109
+        -109 <= target <= 109
+        Only one valid answer exists.
+ ****************************************** END ******************************************/
+public class Problem2 {
+    public static void twoSum(int[] nums, int target) {
+        for(int i = 0; i <= nums.length - 1; i++) {
+            for(int j = i + 1; j <= nums.length - 1; j++) {
+                if(nums[i] + nums[j] == target) {
+                    System.out.println("i : " + nums[i] + " j : " + nums[j]);
+                    System.out.println("index : " + i + " index : " + j);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        twoSum(nums, 9);
+    }
+}
+
+
